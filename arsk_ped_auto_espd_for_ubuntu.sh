91455e5a657b9dec069acce11e1266b5b58ba42a
Crimora <93378732+Crimora@users.noreply.github.com>
2022-11-21 10:59:12 -0800

    Update arsk_ped_auto_espd_for_ubuntu.sh

--- arsk_ped_auto_espd_for_ubuntu.sh
+++ arsk_ped_auto_espd_for_ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# 1) Указать прокси для http, https и ftp (gsettings)
-# 2) Выполнить аналогичные действия и для apt (proxy02) (echo "smth" >> proxy02)
-# 3) Указать DNS (echo nameserver "smth" >> /path_to/interfaces)
-# 4) Скачать и установить сертификат (wget, cp, chown, mkdir, dpkg-reconfigure, update-ca-certificates)
-# 5) Выполнить реконнект, а лучше ребут
+# (ОТМЕНА, см. 2 п.) Указать прокси для http, https и ftp (gsettings)
+# (ОТМЕНА, см. 2 п.) Выполнить аналогичные действия и для apt (proxy02) (echo "smth" >> proxy02)
+# 1) Указать DNS (echo nameserver "smth" >> /path_to/interfaces)
+# 2) (Упрощение) Экспортируем перемменнные среды отвечающие за прокси (export HTTP_PROXY=kakoi.tam.proksi.ya_xz)
+# 3) Скачать и установить сертификат (wget, cp, chown, mkdir, dpkg-reconfigure, update-ca-certificates)
+# 4) Выполнить реконнект, а лучше ребут
